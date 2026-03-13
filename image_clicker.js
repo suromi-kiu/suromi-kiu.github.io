bf4d46ea0d296ee657db599a103d4149b5447982
suromip <kirito0160218@gmail.com>
2026-03-12 22:32:54 -0600

    Image V1

---
+++ image_clicker.js
@@ -0,0 +1,21 @@
+<script>
+
+function openImage(img){
+    const viewer = document.getElementById("image-viewer");
+    const fullscreen = document.getElementById("fullscreen-img");
+
+    fullscreen.src = img.src;
+    viewer.style.display = "flex";
+}
+
+function closeImage(){
+    document.getElementById("image-viewer").style.display = "none";
+}
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape"){
+        closeImage();
+    }
+});
+
+</script>
